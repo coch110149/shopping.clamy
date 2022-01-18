ebfbdfaa16d8bf7c7b0643e8f71872f26e7ad065
Clint <clinton.edward.cochrane@gmail.com>
2022-01-17 19:26:03 -0600

    complete recycler view by way of geeksforgeeks.org

--- app/src/main/java/com/clamy/shopping/MainActivity.kt
+++ app/src/main/java/com/clamy/shopping/MainActivity.kt
@@ -1,58 +1,21 @@
 package com.clamy.shopping
-
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.SparseBooleanArray
-import android.view.LayoutInflater
-import android.widget.ArrayAdapter
-import com.clamy.shopping.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        // Initializing the array lists and the adapter
-        var itemlist = arrayListOf<String>()
-        var adapter =ArrayAdapter<String>(this,
-            android.R.layout.simple_list_item_multiple_choice
-            , itemlist)
-        // Adding the items to the list when the add button is pressed
-        binding.add.setOnClickListener {
+        setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val data = ArrayList<ItemsViewModel>()
 
-            itemlist.add(binding.editText.text.toString())
-            binding.listView.adapter =  adapter
-            adapter.notifyDataSetChanged()
-            // This is because every time when you add the item the input space or the eidt text space will be cleared
-            binding.editText.text.clear()
+        for(i in 1..20){
+            data.add(ItemsViewModel(R.drawable.ic_launcher_background, "Item+i"))
         }
-        // Clearing all the items in the list when the clear button is pressed
-        binding.clear.setOnClickListener {
-
-            itemlist.clear()
-            adapter.notifyDataSetChanged()
-        }
-        // Adding the toast message to the list when an item on the list is pressed
-        binding.listView.setOnItemClickListener { adapterView, view, i, l ->
-            android.widget.Toast.makeText(this, "You Selected the item --> "+itemlist.get(i), android.widget.Toast.LENGTH_SHORT).show()
-        }
-        // Selecting and Deleting the items from the list when the delete button is pressed
-        binding.delete.setOnClickListener {
-            val position: SparseBooleanArray = binding.listView.checkedItemPositions
-            val count = binding.listView.count
-            var item = count - 1
-            while (item >= 0) {
-                if (position.get(item))
-                {
-                    adapter.remove(itemlist.get(item))
-                }
-                item--
-            }
-            position.clear()
-            adapter.notifyDataSetChanged()
-        }
+        val adapter = CustomAdapter(data)
+        recyclerView.adapter = adapter
     }
 }
