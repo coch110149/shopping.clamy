40f899e9f10622f88321d9e8e560ad121faec5a4
Clint <clinton.edward.cochrane@gmail.com>
2022-01-25 20:44:08 -0600

    Created FAB Change Linear Layout to Relative Layout to display FAB add Id to recyclerView

--- app/src/main/java/com/clamy/shopping/MainActivity.kt
+++ app/src/main/java/com/clamy/shopping/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.clamy.shopping
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
         recyclerView.layoutManager = LinearLayoutManager(this)
         val data = ArrayList<ItemsViewModel>()
+
+        val addFab: View = findViewById<RecyclerView>(R.id.add_button)
+        val message = "This will eventually be an action for "
+        addFab.setOnClickListener{ view ->
+            Snackbar.make(view,"$message addFab" , Snackbar.LENGTH_LONG)
+                .setAction("Action", null)
+                .show()
+        }
 
         for(i in 1..20){
             data.add(ItemsViewModel(R.drawable.ic_launcher_background, "Item+i"))
